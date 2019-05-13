67d0e12e3416771db955ec941822897768f51888
Scoot Chacon <Schacon@gmail.com>
2019-05-13 13:13:27 +0200

    Started first function in Esercitazione9

--- Esercitazione9/esercizio.c
+++ Esercitazione9/esercizio.c
@@ -1,13 +1,39 @@
 #include "esercizio.h"
 
+void sottoTree(TipoAlbero a, TipoInfoAlbero *c){
+	if(! estVuoto(sinistro(a))){
+		*c += 1;
+		sottoTree(sinistro(a), c);
+	}
+	if(! estVuoto(destro(a))){
+		*c += 1;
+		sottoTree(destro(a), c);
+	}
+}
+
 TipoInfoAlbero DimensioneAlbero(TipoAlbero a){
-	// IMPLEMENTARE
-	return 0;
+	//Visita in preordine
+	TipoInfoAlbero counter = 0;
+	if(!estVuoto(a)){
+		counter += 1;
+	}
+	//Inizio i controlli ricorsivi sapendo che l'albero non è vuoto alla radice
+	sottoTree(a, &counter);
+	return counter;
+}
+
+void sottoMassimo(TipoAlbero a , TipoInfoAlbero *m){
+	
 }
 
 TipoInfoAlbero TrovaMassimo(TipoAlbero a){
-	// IMPLEMENTARE
-	return 0;
+	//Provo a implementare una visita simmetrica
+	TipoInfoAlbero massimo ;
+	if(! estVuoto(a)){
+		massimo = radice(a);
+	}
+	sottoMassimo(a, &massimo);
+	return massimo;
 }
 
 TipoInfoAlbero contaFraMinMax(TipoAlbero a, const TipoInfoAlbero min, const TipoInfoAlbero max){
@@ -24,6 +50,7 @@ void visitaAlberoBinarioRicerca(TipoAlbero a){
 	// IMPLEMENTARE
 	return;
 }
+
 
 
 TipoAlbero albBinVuoto () {
